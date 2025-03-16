ec04ceafc642e0e16427b086878b4b320b97fdba
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2025-03-16 11:56:50 -0400

    Declare this Project is Under AGPL License

--- LiveHome3dFbxCleanupAndExport.py
+++ LiveHome3dFbxCleanupAndExport.py
@@ -75,6 +75,19 @@
 #   - Object: Bool Tool (https://extensions.blender.org/add-ons/bool-tool/) plug-in enabled.
 #   - Convex Decomposition plug-in (https://github.com/olitheolix/blender-convex-decomposition)
 #     enabled.
+# ==================================================================================================
+# Copyright (C) 2021-2025 Guy Elsmore-Paddock
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+# Affero General Public License as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
 #
 # @author Guy Elsmore-Paddock <guy.paddock@gmail.com>
 # @author Aaron Powell <aaron@lunadigital.tv>
